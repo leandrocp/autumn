da0b408adf2b2ae05ac7f7f3c442e60cb0a226e4
Leandro Pereira <leandro@leandro.io>
2023-09-22 14:12:00 -0400

    update samples

--- lib/mix/tasks/autumn.generate_samples.ex
+++ lib/mix/tasks/autumn.generate_samples.ex
@@ -10,7 +10,7 @@ defmodule Mix.Tasks.Autumn.GenerateSamples do
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Autumn Sample</title>
+    <title>Autumn Sample - <%= @lang %></title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet">
@@ -30,31 +30,33 @@ defmodule Mix.Tasks.Autumn.GenerateSamples do
   </html>
   """
 
+  @langs [
+    {"elixir", ~c"https://raw.githubusercontent.com/elixir-lang/elixir/main/lib/elixir/lib/kernel.ex"},
+    {"rust", ~c"https://raw.githubusercontent.com/tree-sitter/tree-sitter/master/highlight/src/lib.rs"},
+    {"ruby", ~c"https://raw.githubusercontent.com/rack/rack/main/lib/rack/request.rb"}
+  ]
+
   @impl true
   def run(_args) do
-    elixir()
-    rust()
+    :inets.start()
+    :ssl.start()
+
+    for {lang, url} <- @langs do
+      generate(lang, url)
+    end
   end
 
-  defp elixir do
-    Mix.shell().info("Generating sample HTML for Elixir...")
-
-    source = File.read!(Path.join([File.cwd!(), "lib", "autumn", "theme_generator.ex"]))
-
-    code = Autumn.highlight("elixir", source)
-    html = EEx.eval_string(@layout, assigns: %{inner_content: code})
-    dest_path = Path.join([:code.priv_dir(:autumn), "generated", "samples", "elixir.html"])
+  defp generate(lang, url) do
+    Mix.shell().info("Generating sample HTML for #{lang}...")
+    source = download_source(url)
+    code = Autumn.highlight(lang, source)
+    html = EEx.eval_string(@layout, assigns: %{inner_content: code, lang: lang})
+    dest_path = Path.join([:code.priv_dir(:autumn), "generated", "samples", "#{lang}.html"])
     File.write!(dest_path, html)
   end
 
-  defp rust do
-    Mix.shell().info("Generating sample HTML for Rust...")
-
-    source = File.read!(Path.join([File.cwd!(), "native", "autumn", "src", "lib.rs"]))
-
-    code = Autumn.highlight("rust", source)
-    html = EEx.eval_string(@layout, assigns: %{inner_content: code})
-    dest_path = Path.join([:code.priv_dir(:autumn), "generated", "samples", "rust.html"])
-    File.write!(dest_path, html)
+  defp download_source(url) do
+    {:ok, {_, _, body}} = :httpc.request(url)
+    to_string(body)
   end
 end
