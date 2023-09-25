164b25a5adf56c09bd30ede43eac384594650152
Leandro Pereira <leandro@leandro.io>
2023-09-25 10:07:30 -0400

    load all themes

--- lib/mix/tasks/autumn.generate_themes.ex
+++ lib/mix/tasks/autumn.generate_themes.ex
@@ -7,13 +7,17 @@ defmodule Mix.Tasks.Autumn.GenerateThemes do
   @impl true
   def run(_args) do
     Mix.shell().info("Generating themes...")
-
     root_path = Path.join(:code.priv_dir(:autumn), "themes")
     themes = Path.wildcard(root_path <> "/*.toml")
 
     for theme <- themes do
       Mix.shell().info("Parsing #{theme}")
-      ThemeGenerator.generate(theme)
+      ThemeGenerator.generate_theme_file(theme)
     end
+
+    Mix.shell().info("Generating themes.rs")
+    root_path = Path.join([:code.priv_dir(:autumn), "generated", "themes"])
+    themes = Path.wildcard(root_path <> "/*.toml")
+    ThemeGenerator.generate_themes_rs(themes)
   end
 end
