3895b529eb1f708b6f2a3a42ca03744e0a83f8e7
Leandro Pereira <leandro@leandro.io>
2025-04-18 14:16:07 -0400

    feat: add arc and risc targets

--- lib/autumn/native.ex
+++ lib/autumn/native.ex
@@ -38,12 +38,14 @@ defmodule Autumn.Native do
       aarch64-apple-darwin
       aarch64-unknown-linux-gnu
       aarch64-unknown-linux-musl
+      arm-unknown-linux-gnueabihf
+      riscv64gc-unknown-linux-gnu
       x86_64-apple-darwin
-      x86_64-pc-windows-msvc
       x86_64-pc-windows-gnu
+      x86_64-pc-windows-msvc
+      x86_64-unknown-freebsd
       x86_64-unknown-linux-gnu
       x86_64-unknown-linux-musl
-      x86_64-unknown-freebsd
     ),
     variants: %{
       "x86_64-unknown-linux-gnu" => variants_for_linux,
