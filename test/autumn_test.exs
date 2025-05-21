e2b35188f3f65f78782509060c6735b204444f6c
Leandro Pereira <leandro@leandro.io>
2025-05-21 19:26:01 -0400

    fix: test

--- test/autumn_test.exs
+++ test/autumn_test.exs
@@ -74,7 +74,7 @@ defmodule Autumn.AutumnTest do
   end
 
   test "available_themes" do
-    assert Autumn.available_themes() |> length() == 102
+    assert Autumn.available_themes() |> length() == 104
   end
 
   describe "formatter_type: :html_inline" do
